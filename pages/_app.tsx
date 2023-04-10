dda2039e50486a8a50b1e69a1d115be91945b3c1
mbukerepo <mbuke404@gmail.com>
2023-04-10 10:42:02 +0200

    feat: implement the ui of the chatbot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import ChatProvider from "@/context/chat-context";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChatProvider>
+      <Component {...pageProps} />
+    </ChatProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,101 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import remarkGfm from "remark-gfm";
 import ReactMarkdown from "react-markdown";
 import { useTextBuffer } from "../hooks/useTextBuffer";
 import { StreamingText } from "../components/StreamingText";
-const inter = Inter({ subsets: ["latin"] });
+import { Head } from "next/document";
+import Celo from "../public/celo-gpt.webp";
+import Send from "@/components/vectors/Send";
+import { useChat } from "@/context/chat-context";
+import AIMessage from "@/components/AIMessage";
+import User from "@/components/vectors/User";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  display: "swap",
+});
 
 export default function Home() {
-  const [answer, setAnswer] = useState("");
-  const [query, setQuery] = useState(
-    "The easiest way to access ContractKit in your React applications."
-  );
-  const { buffer, done, refresh, cancel } = useTextBuffer({
-    url: "/api/celo-gpt",
-    throttle: 100,
-    method: "POST",
-    data: query,
-  });
-
+  const [message, setMessage] = useState("");
+  const { messages, sendMessage } = useChat();
+  useEffect(() => {
+    console.log(messages);
+  }, [messages]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <StreamingText buffer={buffer} />
-    </main>
+    <>
+      <head>
+        <title>celo-gpt</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <main className="flex bg-[#FCF6F1] h-screen flex-col items-center justify-between p-24-">
+        <main
+          className={`min-h-screen px-4  bg-gray-400- place-content-center grid min-w-[100vw]- ${poppins.className}`}
+        >
+          <div className="max-w-[800px] relative h-full  min-w-[380px] border-3 border-[#331E3F] bg-[#E7E3D4] text-gray-900 shadow-lg md:rounded-md py-8 flex  mx-auto">
+            <div className="self-end w-full pb-12 flex h-[73vh] md:h-[65vh] overflow-scroll scroll-hidden flex-col justify-between">
+              <div className="flex flex-col gap-3">
+                <div className="flex gap-3 px-5">
+                  <Image
+                    className="w-[40px]  h-[40px] rounded-lg"
+                    src={Celo}
+                    alt="celo logo"
+                  />
+                  <p className="max-w-[400px]-">
+                    Hi there! I'm <span className="font-bold">celo-gpt</span>, a
+                    chatbot powered by chatGPT. I read the entire update version
+                    of the Celo documentation. Ask me anything about Celo
+                    ecosystem.
+                  </p>
+                </div>
+                <div className="flex flex-col gap-3">
+                  {messages.map((msg: { role: string; content: string }) =>
+                    msg.role === "user" ? (
+                      <p className="bg-[#FCF6F1] p-5 flex gap-5">
+                        <div>
+                          <User className="w-[30px]  h-[30px] text-gray-600" />
+                        </div>
+                        {msg.content}
+                      </p>
+                    ) : (
+                      <div className="p-5 flex gap-3">
+                        <Image
+                          className="w-[40px]  h-[40px] rounded-lg"
+                          src={Celo}
+                          alt="celo logo"
+                        />
+                        <AIMessage query={msg.content} />
+                      </div>
+                    )
+                  )}
+                </div>
+              </div>
+            </div>
+            <form
+              className="absolute flex bottom-5 max-w-md left-1/2 -translate-x-1/2 w-full"
+              onSubmit={(e) => {
+                sendMessage(e, message);
+                setMessage("");
+              }}
+            >
+              <input
+                value={message}
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+                placeholder="Type a message..."
+                className="outline-none max-w-md mx-auto w-full h-12 px-4 rounded-lg border bg-[#FCF6F1] border-gray-300"
+              />
+              <button type="submit">
+                <Send className="absolute right-2 text-gray-600 top-1/2 -translate-y-1/2" />
+              </button>
+            </form>
+          </div>
+        </main>
+      </main>
+    </>
   );
 }
