3f549c96a40e963ab1459956a0c23f02f6178c5d
mbukerepo <mbuke404@gmail.com>
2023-04-20 17:34:11 +0200

    fix: fix cors error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://celo-gpt.vercel.app/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
